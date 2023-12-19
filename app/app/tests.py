fd66f8f29861d6f8e76621778e983c3b39bdeb8b
Yarin <yarin.software@gmail.com>
2023-12-19 16:14:09 +0200

    Installed PostgreSQL adapter.

--- app/app/tests.py
+++ app/app/tests.py
@@ -4,7 +4,8 @@ Sample tests
 
 from django.test import SimpleTestCase
 
-from app import calc  
+from app import calc 
+
 
 class CalcTests(SimpleTestCase):
     """Tests the clac module"""
@@ -14,7 +15,7 @@ class CalcTests(SimpleTestCase):
         res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
-    
+
     def test_subtract_numbers(self):
         """Test that values are subtracted and returned"""
         res = calc.subtract(10, 15)
