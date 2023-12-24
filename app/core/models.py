56b269ca25217680fac91f884e472cce91908dc7
Yarin <yarin.software@gmail.com>
2023-12-24 19:40:10 +0200

    Implemeted user model and makemigrations

--- app/core/models.py
+++ app/core/models.py
@@ -1,1 +1,35 @@
-from django.contrib import admin  # noqa
+"""
+Database models.
+"""
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+
+
+class UserManager(BaseUserManager):
+    """Manager for user profiles."""
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create, save and return a new user."""
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system."""
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    # custom user manager for our user model (above) to work with django cli commands (createsuperuser) and django admin (user model)
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
