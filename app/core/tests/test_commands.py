e2953bd654ccd0d083ede7bed3cb602708708321
Yarin <yarin.software@gmail.com>
2023-12-24 11:36:45 +0200

    Added wait_for_db command.

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -11,35 +11,33 @@ from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
+
 # mocking this command noted inside patch
-@patch('core.management.commands.wait_for_db.Command.check')
+@patch("core.management.commands.wait_for_db.Command.check")
 class CommandTests(SimpleTestCase):
     """Test commands"""
 
-
     def test_wait_for_db_ready(self, patched_check):
         """Test waiting for db if db is ready"""
-    
+
         patched_check.return_value = True
-        call_command('wait_for_db')
-        patched_check.assert_called_once_with(databases=['default'])
-    
+        call_command("wait_for_db")
+        patched_check.assert_called_once_with(databases=["default"])
+
     # raise exception instead of just returning true
     # keep on vars order
-    @patch('time.sleep')
+    @patch("time.sleep")
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """
         Test waiting for db when getting operational error
         """
-        # first 2 times for psycopg2 error, and after 3 times it's operational error.
-        patched_check.side_effect = [Psycopg2OpError] * 2 + \
-            [OperationalError] * 3 + [True]
+        # first 2 times raise psycopg2 error and then raise 3 operational error
+        patched_check.side_effect = (
+            [Psycopg2OpError] * 2 + [OperationalError] * 3 + [True]
+        )
 
-        
-        call_command('wait_for_db')
+        call_command("wait_for_db")
         # based on the expections before, no more and no less
         self.assertEqual(patched_check.call_count, 6)
-        #checking for default db called
-        patched_check.assert_called_with(databases=['default'])
-        
-        
+        # checking for default db called
+        patched_check.assert_called_with(databases=["default"])
