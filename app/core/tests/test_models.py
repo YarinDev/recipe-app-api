9cb85d2b2a1911b7bedcc331ac1bc0f09e482eb4
Yarin <yarin.software@gmail.com>
2023-12-25 14:08:54 +0200

    Tested user model.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -12,9 +12,33 @@ class ModelTests(TestCase):
         """Test creating a new user with an email is successful"""
         email = "test@example.com"
         password = "testpass123"
-        user = get_user_model().objects.create_user(
-            email=email, password=password)
+        user = get_user_model().objects.create_user(email=email, password=password)
 
         # check if user is created
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test the email for a new user is normalized"""
+        sample_emails = [
+            ["test1@EXAMPLE.com", "test1@example.com"],
+            ["Test2@EXAMPLE.com", "Test2@example.com"],
+            ["TEST3@EXAMPLE.COM", "TEST3@example.com"],
+            ["test4@example.COM", "test4@example.com"],
+        ]
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email=email, password="test123")
+            self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        """Test creating user without email raises ValueError"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(email=None, password="test123")
+
+    def test_create_new_superuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            email="test@example.com, password=test123"
+        )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
